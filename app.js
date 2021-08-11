1b665339b00f1f076c96e63f6676458ccec07d11
Kate Cook <kecook@gmail.com>
2021-08-10 20:56:13 -0400

    created my own Js

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-$(document).ready(function () {
-  /* -----------------------------------------------
-                    MOBILE NAVIGATION     
-    -------------------------------------------------- */
+// $(document).ready(function () {
+//   /* -----------------------------------------------
+//                     MOBILE NAVIGATION
+//     -------------------------------------------------- */
 
-  $('.burger-icon').on('click', function () {
-    if ($('.main-nav').hasClass('mobile-hide')) {
-      $('.main-nav').removeClass('mobile-hide');
-      $('.main-nav').addClass('mobile-show');
-    } else {
-      $('.main-nav').addClass('mobile-hide');
-      $('.main-nav').removeClass('mobile-show');
-    }
+//   $('.burger-icon').on('click', function () {
+//     if ($('.main-nav').hasClass('mobile-hide')) {
+//       $('.main-nav').removeClass('mobile-hide');
+//       $('.main-nav').addClass('mobile-show');
+//     } else {
+//       $('.main-nav').addClass('mobile-hide');
+//       $('.main-nav').removeClass('mobile-show');
+//     }
+//   });
+// });
+window.onload = function () {
+  const hamburger = document.getElementById('burger-icon');
+  const navUL = document.getElementById('nav-drop');
+
+  hamburger.addEventListener('click', () => {
+    navUL.classList.toggle('show');
   });
-});
+};
